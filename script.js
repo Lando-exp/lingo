cd4dc6d0d883d46206645e566c9f047fad724433
Lando-exp <link23.061@gmail.com>
2025-04-14 20:10:39 +0200

    prova

--- script.js
+++ script.js
@@ -1,139 +1,145 @@
 var parole = [
-    "maree", "isola", "piano", "verde", "gatto",
-    "libro", "tavolo", "sedia", "porta", "finestra",
-    "albero", "fiore", "cielo", "vento", "acqua",
-    "fuoco", "terra", "notte", "giorno", "amico",
-    "amore", "cuore", "mano", "donna", "uomo",
-    "tempo", "sogno", "canto", "rumore", "odore"
-];
+	"maree",
+	"isola",
+	"piano",
+	"verde",
+	"gatto",
+	"libro",
+	"tavolo",
+	"sedia",
+	"porta",
+	"finestra",
+	"albero",
+	"fiore",
+	"cielo",
+	"vento",
+	"acqua",
+	"fuoco",
+	"terra",
+	"notte",
+	"giorno",
+	"amico",
+	"amore",
+	"cuore",
+	"mano",
+	"donna",
+	"uomo",
+	"tempo",
+	"sogno",
+	"canto",
+	"rumore",
+	"odore",
+]
 
 var nTentativi = 0
 
 function preparaGioco() {
-    // stampare griglia
-    stampaGriglia()
+	// stampare griglia
+	stampaGriglia()
 
+	// scegliere la parola
+	scegliParola(parole)
 
-    // scegliere la parola
-    scegliParola(parole)
+	//prova
 
-
-    stampaLettereIndizio()
-
-
+	stampaLettereIndizio()
 }
 
 function stampaLettereIndizio() {
-    let parola = localStorage.getItem("soluzione")
-    console.log(parola)
+	let parola = localStorage.getItem("soluzione")
+	console.log(parola)
 
-    let letteraSuggerita1 = ""
-    let letteraSuggerita2 = ""
+	let letteraSuggerita1 = ""
+	let letteraSuggerita2 = ""
 
-    arrayParola = parola.split("")
+	arrayParola = parola.split("")
 
-    letteraSuggerita1 = arrayParola[nCasuale(arrayParola)]
-    console.log("lettera suggerita 1:", letteraSuggerita1)
+	letteraSuggerita1 = arrayParola[nCasuale(arrayParola)]
+	console.log("lettera suggerita 1:", letteraSuggerita1)
 
-    do {
-        letteraSuggerita2 = arrayParola[nCasuale(arrayParola)]
+	do {
+		letteraSuggerita2 = arrayParola[nCasuale(arrayParola)]
 
-        console.log("lettera suggerita 2:", letteraSuggerita2)
+		console.log("lettera suggerita 2:", letteraSuggerita2)
+	} while (letteraSuggerita1 == letteraSuggerita2)
 
-    } while (letteraSuggerita1 == letteraSuggerita2)
+	let parolaConIndizi = ["", "", "", "", ""]
 
+	parolaConIndizi[trovaIndice(arrayParola, letteraSuggerita1)] = letteraSuggerita1
+	parolaConIndizi[trovaIndice(arrayParola, letteraSuggerita2)] = letteraSuggerita2
 
-    let parolaConIndizi = ["", "", "", "", ""]
+	localStorage.setItem("parolaConIndizi", JSON.stringify(parolaConIndizi))
+	parolaConIndizi = JSON.parse(localStorage.getItem("parolaConIndizi"))
 
-    parolaConIndizi[trovaIndice(arrayParola, letteraSuggerita1)] = letteraSuggerita1
-    parolaConIndizi[trovaIndice(arrayParola, letteraSuggerita2)] = letteraSuggerita2
+	console.log("parolaConIndizi", parolaConIndizi)
 
-    localStorage.setItem("parolaConIndizi", JSON.stringify(parolaConIndizi))
-    parolaConIndizi = JSON.parse(localStorage.getItem("parolaConIndizi"))
+	for (let r = 0; r < 6; r++) {
+		for (let c = 0; c < 5; c++) {
+			nCella = "#cella_" + r + "_" + c
 
-    console.log("parolaConIndizi", parolaConIndizi)
+			document.querySelector(nCella).value = parolaConIndizi[c]
 
-    for (let r = 0; r < 6; r++) {
-
-        for (let c = 0; c < 5; c++) {
-
-            nCella = "#cella_" + r + "_" + c;
-
-            document.querySelector(nCella).value = parolaConIndizi[c]
-            
-            /*
+			/*
             document.querySelector(nCella).readOnly = true
 
              if (document.querySelector(nCella).value == "") {
                  document.querySelector(nCella).readOnly = false
              }
             */
-        }
-    }
+		}
+	}
 }
 
-
 function tentativi() {
-    let tentativo = ""
+	let tentativo = ""
 
-    for (let r = 0; r <= nTentativi; r++) {
+	for (let r = 0; r <= nTentativi; r++) {
+		for (let c = 0; c < 5; c++) {
+			nCella = "#cella_" + nTentativi + "_" + c
 
+			if (document.querySelector(nCella) == "") {
+				alert("errore: uno o più campi sono vuoti")
+			} else {
+				tentativo += document.querySelector(nCella).value
+			}
+		}
+	}
 
-        for (let c = 0; c < 5; c++) {
+	console.log("tentativoooo", tentativo)
+}
 
-            nCella = "#cella_" + nTentativi + "_" + c;
+function nCasuale(parola) {
+	let nCasuale = Math.floor(Math.random() * parola.length)
+	return nCasuale
+}
 
-            if(document.querySelector(nCella) == "") {
-                alert("errore: uno o più campi sono vuoti")
-            } else {
-                tentativo += document.querySelector(nCella).value
-            }
-        }
-    }
+function trovaIndice(array, elemento) {
+	return array.indexOf(elemento)
+}
 
-    console.log("tentativoooo", tentativo)
+function scegliParola(parole) {
+	// memorizzare nel local storage
+	let parolaScelta = parole[nCasuale(parole)]
+	console.log(parolaScelta)
 
-    }
+	localStorage.setItem("soluzione", parolaScelta)
+}
 
+function stampaGriglia() {
+	let input = ""
+	let nCella = ""
 
-    function nCasuale(parola) {
-
-        let nCasuale = Math.floor(Math.random() * parola.length)
-        return nCasuale
-    }
-
-    function trovaIndice(array, elemento) {
-        return array.indexOf(elemento);
-    }
-
-
-    function scegliParola(parole) {
-
-        // memorizzare nel local storage
-        let parolaScelta = parole[nCasuale(parole)]
-        console.log(parolaScelta)
-
-        localStorage.setItem("soluzione", parolaScelta)
-
-    }
-
-    function stampaGriglia() {
-        let input = ""
-        let nCella = ""
-
-        for (let r = 0; r < 6; r++) {
-
-
-            for (let c = 0; c < 5; c++) {
-                nCella = "cella_" + r + "_" + c;
-                input += `
+	for (let r = 0; r < 6; r++) {
+		for (let c = 0; c < 5; c++) {
+			nCella = "cella_" + r + "_" + c
+			input += `
                     <label>
                         <input id="${nCella}" type="text" maxlength="1" pattern="[A-Za-z]" style="text-transform: uppercase;">
                     </label>
                     `
-            }
-            input += "<br><br>"
-        }
-        document.querySelector("#input").innerHTML = input
-        input += "<br>"
-    }
+		}
+		input += "<br><br>"
+	}
+	document.querySelector("#input").innerHTML = input
+	input += "<br>"
+}
